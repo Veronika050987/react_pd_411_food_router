e1050b9009f1674d6015ab35181c7f0ebab8db67
Veronika050987 <veronicaviv@mail.ru>
2026-03-09 23:59:18 +0300

    Food site in progress

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,17 @@
+import './CategoryItem.css';
+
+function CategoryItem(props)
+{
+    const {strCategory, strCategoryThumb, strCategoryDescription} = props;
+    return(
+        <div className='card'>
+            <img src={strCategoryThumb} alt={strCategory} />
+            <h2>{strCategory}</h2>
+            <p>{strCategory}</p>
+            <div className='card-action'>
+                <a href='#1'>Watch category</a>
+            </div>
+        </div>
+    )
+}
+export default CategoryItem;
---
+++ src/pages/Layout.js
@@ -0,0 +1,19 @@
+import './Layout.css';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import { Outlet } from 'react-router-dom';
+
+function Layout()
+{
+    return(
+        <div>
+            <Header />
+            <main className='content'>
+                <Outlet />
+                {/* отображает дочерние маршруты в родительском компоненте */}
+            </main>
+            <Footer />
+        </div>
+    )
+}
+export default Layout;
